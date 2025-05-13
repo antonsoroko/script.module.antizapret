60cda6bf40ad81c10ce04b08e62df15419e0fc05
Anton Soroko <anton.soroko@gmail.com>
2025-05-13 15:23:48 +0300

    Version 1.0.0

---
+++ README.md
@@ -0,0 +1,15 @@
+# script.module.antizapret
+
+## Description
+Kodi add-on: Python module helps add-ons to bypass web sites blocking with help of Antizapret proxy. Works on both Python 3 and 2.
+Дополнение для Kodi: этот модуль Python помогает дополнениям обходить блокировку web сайтов с помощью прокси Antizapret. Работает как на Python 3, так и на Python 2.
+
+## Download
+Download .zip file from [Releases](https://github.com/antonsoroko/script.module.antizapret/releases).
+
+## Credits
+Initial idea belongs to https://github.com/afedchin/antizapret
+
+And of course this is possible only because of [Antizapret](https://antizapret.prostovpn.org/)
+
+Initially developed for https://github.com/elgatito/script.elementum.burst
---
+++ example/antizapret_test.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+sys.path.insert(0, os.path.join(os.path.relpath(os.path.dirname(__file__), "./"), '..', 'lib'))
+
+import antizapret
+import requests
+from six.moves import urllib_parse
+
+
+if __name__ == "__main__":
+    antizapret_proxy = antizapret.AntizapretProxy()
+    # Test cases
+    print("Testing proxy detection:")
+    torrent_trackers = [
+        "rutracker.org",
+        "rutracker.net",
+        "rutor.info",
+        "rutor.is",
+        "nnmclub.to",
+        "kinozal.tv",
+        "rustorka.com",
+        "megapeer.vip",
+        "bluebird-hd.org",
+        "pirat.one",
+        "www.lostfilm.tv",
+        "www.lostfilm.download",
+        "newstudio.tv",
+        "le-production.tv",
+        "anilibria.tv",
+        "anilibria.top",
+        "anilibria.wtf",
+        "tr.anidub.com",
+        "animaunt.fun",
+        "tracker.0day.community",
+        "baibako.tv",
+        "toloka.to",
+        "thepiratebay.org",
+        "thepiratebay10.xyz",
+        "1337x.to",
+        "glodls.to",
+        "yts.mx",
+        "eztvx.to",
+        "bt4gprx.com",
+        "bitsearch.to",
+        "torrenting.com",
+        "www.limetorrents.lol",
+        "uindex.org",
+    ]
+    for domain in torrent_trackers:
+        print("{} -> {}".format(domain, antizapret_proxy.detect(domain)))
+    print("-" * 20)
+
+    test_urls = ["https://rutracker.org", "https://rutracker.net/forum/viewtopic.php?t=5324346", "https://rutor.info/torrent/472", "https://rutor.is", "https://kinozal.tv", "https://www.lostfilm.tv:443", "https://ifconfig.co:443/ip"]
+
+    for url in test_urls:
+        print("\nTesting opening URL: %s" % (url))
+        parsed = urllib_parse.urlparse(url)
+        proxy = antizapret_proxy.detect(host=parsed.netloc)
+        print("Detected Antizapret proxy for %s: %s" % (parsed.netloc, proxy))
+        proxies = {
+            'http': proxy,
+            'https': proxy,
+        }
+        try:
+            response = requests.get(url, proxies=proxies)
+            print("Code: %s" % (response.status_code))
+            body = response.text
+            if len(body) < 100:
+                print("Body: %s" % (body))
+            else:
+                print("Body length: %s" % (len(body)))
+        except Exception as e:
+            print("Can't make a request: %s" % (e))
